ffbf002c392ddc007cb164dd37716483577da096
hugosaner <hugo@sanerdesign.com>
2012-07-25 17:45:53 +0100

    removed comments function from functions.php to be placed in themes functions to allow design changes in different themes
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- functions.php
+++ functions.php
@@ -78,58 +78,6 @@ function response_content_nav( $nav_id ) {
 }
 endif; // response_content_nav
 
-if ( ! function_exists( 'response_comment' ) ) :
-// FIXME: Fix documentation
-// Template for comments and pingbacks.
-// Used as a callback by wp_list_comments() for displaying the comments.
-function response_comment( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
-	switch ( $comment->comment_type ) :
-		case 'pingback' :
-		case 'trackback' :
-	?>
-	<li class="post pingback">
-		<p><?php _e( 'Pingback:', 'response' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'response' ), ' ' ); ?></p>
-	<?php
-			break;
-		default :
-	?>
-	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
-		<article id="comment-<?php comment_ID(); ?>" class="comment">
-			<footer>
-				<div class="comment-author vcard">
-					<?php echo get_avatar( $comment, 40 ); ?>
-					<?php printf( __( '%s <span class="says">says:</span>', 'response' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
-				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em><?php _e( 'Your comment is awaiting moderation.', 'response' ); ?></em>
-					<br />
-				<?php endif; ?>
-
-				<div class="comment-meta commentmetadata">
-					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
-					<?php
-						/* translators: 1: date, 2: time */
-						printf( __( '%1$s at %2$s', 'response' ), get_comment_date(), get_comment_time() ); ?>
-					</time></a>
-					<?php edit_comment_link( __( '(Edit)', 'response' ), ' ' );
-					?>
-				</div><!-- .comment-meta .commentmetadata -->
-			</footer>
-
-			<div class="comment-content"><?php comment_text(); ?></div>
-
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
-		</article><!-- #comment-## -->
-
-	<?php
-			break;
-	endswitch;
-}
-endif; // ends check for response_comment()
-
 if ( ! function_exists( 'response_posted_on' ) ) :
 // FIXME: Fix documentation
 //Prints HTML with meta information for the current post-date/time and author.
