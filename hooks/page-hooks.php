6f775cf3a590c2cbc299962ef4b82ba6aa6b00f5
hugosaner <hugo@sanerdesign.com>
2012-10-16 15:53:46 +0100

    Page Comments - Changed code slightly to match single posts
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- hooks/page-hooks.php
+++ hooks/page-hooks.php
@@ -46,7 +46,11 @@ function cyberchimps_page(){ ?>
 
 			<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php comments_template( '', true ); ?>
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() )
+					comments_template( '', true );
+			?>
 
 		<?php endwhile; // end of the loop. ?>
 	
