eb1a7d806e3ad2789063b84a7afa6a28cde2eeeb
hugosaner <hugo@sanerdesign.com>
2012-10-31 13:37:21 +0000

    header hooks - removed navigation as we don't use it
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- hooks/header-hooks.php
+++ hooks/header-hooks.php
@@ -230,24 +230,6 @@ function cyberchimps_header_social_icons() {
 	
 <?php }
 
-// Navigation
-function cyberchimps_navigation() { ?>
-	<div class="container">
-		<div class="row">
-			<div class="twelve columns" id="menu">
-				<div id="nav" class="twelve columns">
-					<?php wp_nav_menu( array( 
-						'theme_location' => 'header-menu', // Setting up the location for the main-menu, Main Navigation.
-						'fallback_cb' => 'cyberchimps_menu_fallback', //if wp_nav_menu is unavailable, WordPress displays wp_page_menu function, which displays the pages of your blog.
-						'items_wrap' => '<ul id="nav_menu">%3$s</ul>',
-					)); ?>
-				</div>
-			</div>
-		</div>
-	</div>
- <?php
-}
-
 // Custom HTML header element.
 function cyberchimps_custom_header_element_content() { ?>
 	<header class="row-fluid">
