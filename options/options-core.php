4e20861ba11d32d2bf4c6f000604dc357dd9a182
mfssurya <suryap@mindfiresolutions.com>
2012-12-14 12:09:24 +0530

    Added toggle for responsiveness of menu.

--- options/options-core.php
+++ options/options-core.php
@@ -410,6 +410,15 @@ function cyberchimps_add_core_fields( $fields_list ) {
 		'section' => 'cyberchimps_custom_layout_section',
 		'heading' => 'cyberchimps_design_heading'
 	);
+	
+	// Toggle for responsive menu
+	$fields_list[] = array(
+		'name' => __('Responsive Menu', 'cyberchimps'),
+		'id' => 'responsive_menu',
+		'type' => 'toggle',
+		'section' => 'cyberchimps_custom_layout_section',
+		'heading' => 'cyberchimps_design_heading'
+	);
 		
 	$fields_list[] = array(
 		'name' => __('Max Width', 'cyberchimps'),
