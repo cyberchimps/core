d2241b30e4e2b0e084d16dc1a49d7b4e1f5bed3f
Hugo Saner <hugo@sanerdesign.com>
2013-10-29 09:40:01 +0000

    footer seo fix

--- hooks/footer-hooks.php
+++ hooks/footer-hooks.php
@@ -29,7 +29,7 @@ function cyberchimps_footer_credit() {
 					<div class="span6">
 						<div id="credit">
 							<?php if( cyberchimps_get_option( 'footer_cyberchimps_link', 1 ) == '1' ): ?>
-								<a href="http://cyberchimps.com/" target="_blank" title="CyberChimps Wordpress themes">
+								<a href="http://cyberchimps.com/" target="_blank" title="CyberChimps Themes">
 									<img class="cc-credit-logo" src="<?php echo get_template_directory_uri(); ?>/cyberchimps/lib/images/achimps.png" alt="CyberChimps"/>
 									<h4 class="cc-credit-text"><span>Cyber</span>Chimps</h4>
 								</a>
