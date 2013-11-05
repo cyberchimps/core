38af37411fa9dcc3629e528bea6b7a889a1c7100
Hugo Saner <hugo@sanerdesign.com>
2013-11-05 11:06:09 +0000

    translation - added another missing text domain

--- options/meta-box-class/my-meta-box-class.php
+++ options/meta-box-class/my-meta-box-class.php
@@ -1986,7 +1986,7 @@ if( !class_exists( 'AT_Meta_Box' ) ) :
 						return $file;
 					}
 				}
-				$file['error'] = __( "Sorry, you cannot upload this file type for this field." );
+				$file['error'] = __( 'Sorry, you cannot upload this file type for this field.', 'cyberchimps_core' );
 			}
 
 			return $file;
