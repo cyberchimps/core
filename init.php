4f171dcb07d441594afb31e6f7971f4e65f06d30
Simon S <simon@cyberchimps.com>
2014-02-12 15:38:37 +0530

    Removed theme level text domain from core.

--- init.php
+++ init.php
@@ -61,7 +61,6 @@ if ( !function_exists( 'cyberchimps_core_setup_theme' ) ):
 		// Core Translations can be filed in the /inc/languages/ directory
 		load_theme_textdomain( 'cyberchimps_core', $directory . '/cyberchimps/lib/languages' );
 		load_theme_textdomain( 'cyberchimps_elements', $directory . '/elements/lib/languages' );
-		load_theme_textdomain( 'cyberchimps', $directory . '/inc/languages' );
 
 		// Add support for the Aside Post Formats
 		add_theme_support( 'post-formats', array( 'aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat' ) );
