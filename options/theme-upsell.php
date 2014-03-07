0f3b4a9ee27c159be9ad406edf4280d57cabd00f
Hugo Saner <hugo@sanerdesign.com>
2014-03-07 14:12:18 +0000

    Styling for upsell description

--- options/theme-upsell.php
+++ options/theme-upsell.php
@@ -41,10 +41,10 @@ add_action( 'admin_menu', 'cyberchimps_add_upsell' );
 
 // Define markup for the upsell page.
 function cyberchimps_display_upsell() {
-	
+
 	// Set template directory uri
 	$directory_uri = get_template_directory_uri();
-?>
+	?>
 	<div class="wrap">
 		<div class="container-fluid">
 			<div id="upsell_container">
@@ -55,106 +55,108 @@ function cyberchimps_display_upsell() {
 								<img src="<?php echo $directory_uri; ?>/cyberchimps/options/lib/images/options/upsell-logo.png"/>
 							</a>
 						</h2>
+
 						<h3><?php _e( 'Themes You Can Trust', 'cyberchimps_core' ); ?></h3>
 					</div>
 				</div>
-				
+
 				<div id="upsell_themes" class="row-fluid">
 					<?php
 					// Set the argument array with author name.
 					$args = array(
 						'author' => 'cyberchimps',
 					);
-					
+
 					// Set the $request array.
 					$request = array(
 						'body' => array(
-							'action' => 'query_themes',
-							'request' => serialize((object)$args)
+							'action'  => 'query_themes',
+							'request' => serialize( (object)$args )
 						)
 					);
-					$themes = cyberchimps_get_themes( $request );					
+					$themes = cyberchimps_get_themes( $request );
 					$counter = 1;
-					
-					foreach( $themes->themes as $theme ) {
-					
+
+					foreach ( $themes->themes as $theme ) {
+
 						// Set the argument array with author name.
 						$args = array(
 							'slug' => $theme->slug,
 						);
-						
+
 						// Set the $request array.
 						$request = array(
 							'body' => array(
-								'action' => 'theme_information',
-								'request' => serialize((object)$args)
+								'action'  => 'theme_information',
+								'request' => serialize( (object)$args )
 							)
 						);
-					
+
 						$theme_details = cyberchimps_get_themes( $request );
-					?>
-						
+						?>
+
 						<div id="<?php echo $theme->slug; ?>" class="theme-container span4 <?php echo $counter % 3 == 1 ? 'no-left-megin' : ""; ?>">
-							
-							<img class="theme-screenshot" src="<?php echo $theme->screenshot_url ?>"/>
+							<div class="image-container">
+								<img class="theme-screenshot" src="<?php echo $theme->screenshot_url ?>"/>
 							
 							<span class="theme-description">
 								<p><?php echo $theme->description; ?></p>
 							</span>
-							
+							</div>
 							<div class="theme-details">
 								<span class="theme-name"><?php echo $theme->name; ?></span>
-								<a data-toggle="tooltip" data-placement="bottom" title="<?php echo 'Downloaded ' . number_format( $theme_details->downloaded ) . ' times'; ?>" class="button button-primary download right" target="_blank" href="<?php echo $theme->homepage; ?>">Download</a>
+								<a data-toggle="tooltip" data-placement="bottom" title="<?php echo 'Downloaded ' . number_format( $theme_details->downloaded ) . ' times'; ?>"
+								   class="button button-primary download right" target="_blank" href="<?php echo $theme->homepage; ?>">Download</a>
 								<a class="button button-secondary preview right" target="_blank" href="<?php echo $theme->preview_url; ?>">Live Preview</a>
 							</div>
 						</div>
-					<?php
+						<?php
 						$counter++;
 					}?>
 				</div>
 			</div>
 		</div>
 	</div>
-	
-<script>
-	jQuery(function () {
-		jQuery('.download').tooltip();
-	});
-</script>
+
+	<script>
+		jQuery(function () {
+			jQuery('.download').tooltip();
+		});
+	</script>
 <?php
 }
 
 // Get all CyberChimps themes by using API.
 function cyberchimps_get_themes( $request ) {
-	
+
 	// Generate a cache key that would hold the response for this request:
-    $key= 'cyberchimps_' . md5( serialize( $request ) );
-	
+	$key = 'cyberchimps_' . md5( serialize( $request ) );
+
 	// Check transient. If it's there - use that, if not re fetch the theme
-    if ( false === ( $themes = get_transient( $key ) ) ) {
-	
+	if ( false === ( $themes = get_transient( $key ) ) ) {
+
 		// Transient expired/does not exist. Send request to the API.
-		$response = wp_remote_post( 'http://api.wordpress.org/themes/info/1.0/', $request);
-	
+		$response = wp_remote_post( 'http://api.wordpress.org/themes/info/1.0/', $request );
+
 		// Check for the error.
 		if ( !is_wp_error( $response ) ) {
-		
+
 			$themes = unserialize( wp_remote_retrieve_body( $response ) );
-			
+
 			if ( !is_object( $themes ) && !is_array( $themes ) ) {
-			
+
 				// Response body does not contain an object/array
-				return new WP_Error('theme_api_error', 'An unexpected error has occurred');
+				return new WP_Error( 'theme_api_error', 'An unexpected error has occurred' );
 			}
-			
+
 			// Set transient for next time... keep it for 24 hours should be good
-			set_transient($key, $themes, 60*60*24);
+			set_transient( $key, $themes, 60 * 60 * 24 );
 		}
 		else {
 			// Error object returned
 			return $response;
 		}
 	}
-	
+
 	return $themes;
 }
